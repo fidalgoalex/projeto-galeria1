b5d963e7a526fea188775bb30b50f88401528a52
Alex Fidalgo <fidalgo.alex1@gmail.com>
2018-12-27 10:04:40 -0200

    Update GaleriaModel.js

--- rest-api/model/galeria/GaleriaModel.js
+++ rest-api/model/galeria/GaleriaModel.js
@@ -9,4 +9,8 @@ module.exports = class GaleriaModel{
         return db.query("SELECT * FROM galeria WHERE id_galeria =?",
         [id], callback);
     }
+    static adicionar(dados, callback){
+        return db.query("INSERT INTO galeria(titulo, caminho) VALUES(?, ?)",
+        [dados.titulo, dados.caminho], callback);
+    }
 }
